48fcf0fd8e448fd2df6adcb39a7a42fe1fa16a35
José Motta Lopes <josemotta@bampli.com>
2025-03-13 11:26:37 -0300

    Create 2025.03.11.cf.reuniao.m

---
+++ 2025.03.11.cf.reuniao.m
@@ -0,0 +1,28 @@
+# CCRRC - REUNIÃO CONSELHO FISCAL
+
+## Participantes em 13/1/2025 incluem membros CF, síndico e convidado
+
+- Deodoro
+- Gil
+- José Motta
+- William (síndico)
+- Roberto Lemos
+
+## Tópicos abordados em 13/1/2025 pelos membros CF, síndico e convidado:
+
+## Orçamento
+
+## Inadimplentes
+
+## Fundo reserva
+
+## Omo: receita das máquinas lavanderia
+
+## Retornos previstos do William para CF 
+
+- Orçamento 2025
+- Cotas fundo: quais contas temos acesso?
+- Extrato das contas do condomínio & aplicação
+- Relatório inadimplência
+- Jurídico está incluído no orçamento?
+- Quais sub-contas existem? churrasqueira, salão?
